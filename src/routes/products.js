bf0e78a49df0a3ca50e776fc48c83b7d10234a21
Blake Archer <blarcher31@gmail.com>
2022-04-05 10:32:19 -0500

    Added routes for getting products

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,8 +4,23 @@ import db from '../startup/db'
 
 /* Gets ALL products from the database. */
 router.get('/', async (req, res) => {
-  let sql =
-    'SELECT products.id, products.price, products.title, product_img.img_url, products.description FROM products INNER JOIN product_img ON products.img_id = product_img.id'
+  let sql = `SELECT products.id, products.price, products.title, product_img.img_url, products.description, products.category_id 
+    FROM products INNER JOIN product_img ON product_img.product_id = products.id
+    WHERE NOT products.id = 6;`
+  db.query(sql, function (err, result) {
+    if (err) throw err
+    res.send(result)
+  })
+})
+
+/* Get ALL products from a specific category from the db. */
+router.get('/categories/:categoryID', async (req, res) => {
+  let sql = `SELECT products.id, products.price, products.title, product_img.img_url, products.description, product_category.name 
+  FROM products 
+  INNER JOIN product_img ON product_img.product_id = products.id
+  INNER JOIN product_category ON products.category_id = product_category.id
+  WHERE product_category.id = ${req.params.categoryID}`
+
   db.query(sql, function (err, result) {
     if (err) throw err
     console.log(result)
@@ -16,14 +31,9 @@ router.get('/', async (req, res) => {
 /*Gets FEATURED products*/
 
 router.get('/featured', async (req, res) => {
-  // db.connect(function (err) {
-  //   if (err) throw err
-  //   console.log('Connected!')
-  // })
-
-  let sql = `SELECT products.id, products.price, products.title, product_img.img_url
+  let sql = `SELECT products.id, products.price, products.title, product_img.img_url, products.category_id
   From products
-  INNER JOIN product_img ON products.img_id = product_img.id
+  INNER JOIN product_img ON product_img.product_id = products.id
   WHERE featured=TRUE;`
 
   db.query(sql, function (err, result) {
@@ -33,13 +43,58 @@ router.get('/featured', async (req, res) => {
   })
 })
 
-/*Gets a sepecific Product*/
+/*Gets a specific Product*/
 
-router.get('/:id', async (req, res) => {
+router.get('/product/:id', async (req, res) => {
   let sql = `SELECT products.id, products.price, products.title, product_img.img_url, products.description
   From products
-  INNER JOIN product_img ON products.img_id = product_img.id
+  INNER JOIN product_img ON product_img.product_id = products.id
   WHERE products.id=${req.params.id};`
+
+  db.query(sql, function (err, result) {
+    if (err) throw err
+    console.log(result)
+    res.send(result)
+  })
+})
+
+router.get('/cart-items', async (req, res) => {
+  let productIds = JSON.parse(req.headers.productids)
+  let sql = `SELECT products.id, products.title, products.price, product_img.img_url
+  FROM products
+  INNER JOIN product_img ON product_img.product_id = products.id
+  WHERE products.id IN (?);`
+  console.log(productIds)
+
+  db.query(sql, [productIds], function (err, result) {
+    if (err) throw err
+    console.log(result)
+    res.send(result)
+  })
+})
+
+/* This Function Will get all Categories from the database.
+    It will return the category name and IMG URL. */
+
+router.get('/categories', async (req, res) => {
+  let sql = `SELECT product_category.name, product_category.id, product_img.img_url
+      From product_category
+      INNER JOIN product_img ON product_category.img_id = product_img.id;`
+
+  db.query(sql, function (err, result) {
+    if (err) throw err
+    console.log(result)
+    res.send(result)
+  })
+})
+
+/* This Function will get the custom board product from the DB and return
+    the URL for the pictures along with price and title. */
+
+router.get('/custom', async (req, res) => {
+  let sql = `SELECT products.id, products.price, products.title, product_img.img_url, products.description 
+      FROM products INNER JOIN product_img ON product_img.product_id = products.id
+      WHERE products.id = 6;`
 
   db.query(sql, function (err, result) {
     if (err) throw err
